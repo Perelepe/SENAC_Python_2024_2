890e6783ab3144834e7b2b44ff63f8923f21e736
Perelepe <felipe.perep@gmail.com>
2024-07-12 21:41:32 -0300

    Introdução a banco de dados

---
+++ SQL/Aula 1/academia_alunos_cursos.sql
@@ -0,0 +1,7 @@
+UPDATE academia.cursos
+SET descricao="Curso formativo de programação em Python"
+WHERE codigo=1;
+
+UPDATE academia.cursos
+SET descricao="Curso formativo de programação em Java"
+WHERE codigo=2;
