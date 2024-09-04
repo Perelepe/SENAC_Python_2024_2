c6a821e46e538d5dc9ec1c21d18a78aebbc61346
Perelepe <felipe.perep@gmail.com>
2024-09-04 19:38:40 -0300

    Mudanças em aula

--- Python/Projeto_Web/visitantes/views.py
+++ Python/Projeto_Web/visitantes/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
 from django.utils import timezone
+from django.contrib import messages
 from visitantes.forms import ( VisitanteForm, AutorizaVisitanteForm )
 from visitantes.models import Visitante
+from porteiros.models import Porteiro
 
 def registrar_visitantes(request):
-    form = AutorizaVisitanteForm()
+    form = VisitanteForm(request.POST)
 
-    if request.method == "POST":
-        form = AutorizaVisitanteForm(request.POST, instance=visitante)
-        if form.is_valid():
-            visitante = form.save(commit=False)
-            visitante.status = "EM_VISITA"
-            visitante.horario_autorizacao = timezone.now()
-            visitante.save()
-            messages.success(request, "Visitante autorizado com sucesso.")
-            return redirect("index")
+    if form.is_valid():
+        visitante = form.save(commit = False)
+        visitante.porteiro_autorizador = Porteiro.objects.get(id=1)
+        visitante.save()
+
+        messages.sucess(request, "O Visitante foi cadastrado com sucesso!")
+        return redirect("index")
 
     context = {
         "nome_pagina":"Cadastro de Visitante",
@@ -24,30 +23,28 @@ def registrar_visitantes(request):
     return render(request, "visitantes/visitantes.html", context)
 
 def informacoes_visitante(request, id):
-    visitante = get_object_or_404(
-        Visitante,
-        id=id
-    )
+    visitante = get_object_or_404(Visitante, id=id)
+    
+    form = AutorizaVisitanteForm()
+    if request.method == "POST":
+        form = AutorizaVisitanteForm(request.POST, instance=visitante)
+
+        if form.is_valid():
+            visitante = form.save(commit=False)
+
+            visitante.status = "EM_VISITA"
+            visitante.horario_autorizacao = timezone.now()
+
+            visitante.save()
+
+            messages.success(request, "Visitante autorizado com sucesso.")
+        
+        return redirect("index")
+    
     context = {
-        "nome_pagina": "Informações do Visitante",
-        "visitante":visitante,
-        }
+        "nome_pagina":"Informações do Visitante",
+        "visitante":visitante, 
+        "form":form,
+    }
+
     return render(request, "visitantes/informacoes_visitante.html", context)
-
-def finalizar_visita(request, id):
-    if request.method=="POST":
-        visitante = get_object_or_404(
-            Visitante,
-            id=id
-        )
-
-        visitante.status = "FINALIZADO"
-        visitante.horario_saida = timezone.now()
-        
-        visitante.save()
-        
-        messages.success(
-            request,
-            "Visita finalizada com sucesso"
-        )
-    return redirect("index")
